b938be07e3baa9e6ed1ea5fa4b865de7c31bce7f
Erik Jaime Pacheco Arana <devrikoficial@gmail.com>
2021-10-20 15:00:52 -0500

    Estructura inicial de la pagina de list

--- pages/list.vue
+++ pages/list.vue
@@ -1,9 +1,36 @@
 <template>
-  <div class="main-container"></div>
+  <div class="main-container">
+    <form class="form-list" @submit.prevent="searchPokemon">
+      <input
+        type="search"
+        placeholder="Search"
+        class="list-input"
+        v-model.trim="search"
+      />
+
+      <ul class="list-container">
+        <li class="list-item">1</li>
+        <li class="list-item">2</li>
+        <li class="list-item">3</li>
+        <li class="list-item">4</li>
+        <li class="list-item">5</li>
+        <li class="list-item">6</li>
+      </ul>
+    </form>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      search: '',
+    }
+  },
+  methods: {
+    async searchPokemon() {},
+  },
+}
 </script>
 
 <style lang="scss" scoped></style>
